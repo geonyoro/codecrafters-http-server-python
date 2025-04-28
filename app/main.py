5bd3f2ab672088fe2fc1ee1bee93e21465f15f1e
George Nyoro <geonyoro@gmail.com>
2025-04-28 11:33:20 +0300

    feat: Respond for echo endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import re  # noqa: F401
 import socket  # noqa: F401
 
 
@@ -8,9 +9,18 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         sock, _ = server_socket.accept()  # wait for client
-        data = sock.recv(1024)
-        status = b"200 OK" if b"GET / " in data else b"404 Not Found"
-        sock.sendall(b"HTTP/1.1 %s\r\n\r\n" % status)
+        request = sock.recv(1024)
+        if b"GET / " in request:
+            sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif b"GET /echo/" in request:
+            sobj = re.search(rb"/echo/(\S+)", request)
+            if not sobj:
+                sock.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
+            else:
+                response_data = sobj[1]
+                sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n%s\r\n" % response_data)
+        elif b"404 Not Found" in request:
+            sock.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
         sock.close()
 
 

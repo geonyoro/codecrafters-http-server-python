418d71e106a54da9f6190202ff7a72b9b3a58490
George Nyoro <geonyoro@gmail.com>
2025-04-28 11:48:41 +0300

    feat: Attempt refactor

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import re  # noqa: F401
 import socket  # noqa: F401
 
 
@@ -10,20 +9,26 @@ def main():
     while True:
         sock, _ = server_socket.accept()  # wait for client
         request: str = sock.recv(1024).decode()
-        if "GET / " in request:
+        first_line = request.split("\r\n")[0]
+        method, path, _ = first_line.split()
+        if method != "GET":
+            sock.sendall(b"HTTP/1.1 405 Method Not Allowed\r\n\r\nMethod Not Allowed")
+            sock.close()
+            continue
+        if path == "/":
             sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        elif "GET /echo/" in request:
-            sobj = re.search(r"/echo/(\S+)", request)
-            if not sobj:
-                sock.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
-            else:
-                param = sobj[1]
-                response_data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
-                print(f"{param=} {response_data=}")
-                sock.sendall(response_data.encode("utf-8"))
+            sock.close()
+            continue
+        if path.startswith("/echo/"):
+            param = path[6:]
+            response_data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
+            sock.sendall(response_data.encode("utf-8"))
+            sock.close()
+            continue
         else:
             sock.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        sock.close()
+            sock.close()
+            continue
 
 
 if __name__ == "__main__":

41861977d41707718b538f65d435b3ea22b1de41
George Nyoro <geonyoro@gmail.com>
2025-04-28 11:40:45 +0300

    fix: Send encoding properly

--- app/main.py
+++ app/main.py
@@ -9,21 +9,19 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         sock, _ = server_socket.accept()  # wait for client
-        request = sock.recv(1024)
-        if b"GET / " in request:
+        request: str = sock.recv(1024).decode()
+        if "GET / " in request:
             sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        elif b"GET /echo/" in request:
-            sobj = re.search(rb"/echo/(\S+)", request)
+        elif "GET /echo/" in request:
+            sobj = re.search(r"/echo/(\S+)", request)
             if not sobj:
                 sock.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
             else:
                 param = sobj[1]
-                response_data = (
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s"
-                    % (len(param), param)
-                )
+                response_data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}%s\r\n\r\n{param}\n\n"
+                print(f"{param=} {response_data=}")
                 sock.sendall(response_data.encode("utf-8"))
-        elif b"404 Not Found" in request:
+        elif "404 Not Found" in request:
             sock.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
         sock.close()
 
